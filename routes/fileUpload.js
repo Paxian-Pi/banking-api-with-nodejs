64aa90f8ff96f4fb1c6a7ccc4e2512661a01ba31
Paxian-Pi <paxian.pi@gmail.com>
2022-08-01 03:28:14 +0100

    updated

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -17,27 +17,28 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// router.get('/upload', (req, res) => res.send('File upload API'))
+router.get('/upload', (req, res) => res.send('File upload API'))
 
-// router.put("/uploadfile", upload.single("file"), uploadFile);
+router.put("/uploadfile", upload.single("file"), uploadFile);
 
-// function uploadFile(req, res) {
-//     const host = req.hostname;
-//     const filePath = req.protocol + "://" + host + '/' + req.file.path;
+function uploadFile(req, res) {
+    const host = req.hostname;
+    const filePath = req.protocol + "://" + host + '/' + req.file.path;
     
-//     const fileUpload = new FileUploadModel({
-//         file: req.file.filename,
-//         filePath: filePath
-//     })
+    // const fileUpload = new FileUploadModel({
+    //     file: req.file.filename,
+    //     filePath: filePath
+    // })
     
-//     fileUpload
-//         .save()
-//         .then(file => {
-//             console.log(file);
-//             res.json({ message: "Successfully uploaded file", data: file });
-//         })
+    // fileUpload
+    //     .save()
+    //     .then(file => {
+    //         console.log(file);
+    //         res.json({ message: "Successfully uploaded file", data: file });
+    //     })
 
-// }
+    res.json({ message: "Successfully uploaded file", data: req.file });
+}
 
 
 // router.put('/uploadfile', upload.single('file'), (req, res) => {

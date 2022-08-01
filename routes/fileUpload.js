75b7228fc79770c8a6df6b547210c84590acbfa2
Paxian-Pi <paxian.pi@gmail.com>
2022-08-01 02:45:44 +0100

    updated

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -2,16 +2,16 @@ const express = require('express')
 const router = express.Router()
 const multer = require("multer")
 const path = require("path")
-const fs = require("fs")
+// const fs = require("fs")
 
 const FileUploadModel = require('../models/FileUploadModel')
 
 var fileName;
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, __dirname + '/uploads/');
-    },
+    // destination: function (req, file, cb) {
+    //     cb(null, __dirname + '/uploads/');
+    // },
     filename: function (req, file, cb) {
         // fileName = Date.now() + path.extname(file.originalname)
         cb(null, Date.now() + path.extname(file.originalname))
@@ -27,7 +27,7 @@ router.put("/uploadfile", upload.single("file"), uploadFile);
 function uploadFile(req, res) {
     const host = req.hostname;
     const filePath = req.protocol + "://" + host + '/' + req.file.path;
-    
+
     const fileUpload = new FileUploadModel({
         file: req.file.filename,
         filePath: filePath

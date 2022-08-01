6fa1a6b68b8ac86ae6ae1408bfa8392741684a37
Paxian-Pi <paxian.pi@gmail.com>
2022-08-01 04:14:52 +0100

    updated

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -4,7 +4,7 @@ const multer = require("multer")
 const path = require("path")
 // const fs = require("fs")
 
-// const FileUploadModel = require('../models/FileUploadModel')
+const FileUploadModel = require('../models/FileUploadModel')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {

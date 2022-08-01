0b39d3b8eec8651a8249d7fd05ac947562a90781
Paxian-Pi <paxian.pi@gmail.com>
2022-08-01 03:04:28 +0100

    updated

--- routes/accounts.js
+++ routes/accounts.js
@@ -2,15 +2,6 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 
-
-
-const multer = require("multer")
-const path = require("path")
-
-const FileUploadModel = require('../models/FileUploadModel')
-
-
-
 // Load validation
 const validateAccountInput = require('../validation/account')
 
@@ -741,44 +732,5 @@ router.get('/transactions/current-user/:user_id', (req, res) => {
         })
         .catch(err => res.status(400).json(err))
 })
-
-
-
-
-
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, __dirname + '/uploads/');
-    },
-    filename: function (req, file, cb) {
-        // fileName = Date.now() + path.extname(file.originalname)
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-});
-
-const upload = multer({ storage: storage });
-
-router.get('/upload', (req, res) => res.send('File upload API'))
-
-router.put("/uploadfile", upload.single("file"), uploadFile);
-
-function uploadFile(req, res) {
-    const host = req.hostname;
-    const filePath = req.protocol + "://" + host + '/' + req.file.path;
-    
-    const fileUpload = new FileUploadModel({
-        file: req.file.filename,
-        filePath: filePath
-    })
-    
-    fileUpload
-        .save()
-        .then(file => {
-            console.log(file);
-            res.json({ message: "Successfully uploaded file", data: req.file });
-        })
-
-}
 
 module.exports = router

d302b188b9db794557efd790048b6da2dbf04386
Paxian-Pi <paxian.pi@gmail.com>
2022-08-01 03:13:11 +0100

    updated

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -4,43 +4,40 @@ const multer = require("multer")
 const path = require("path")
 // const fs = require("fs")
 
-const FileUploadModel = require('../models/FileUploadModel')
+// const FileUploadModel = require('../models/FileUploadModel')
 
-var fileName;
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, __dirname + '/uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + path.extname(file.originalname))
+//     }
+// });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, __dirname + '/uploads/');
-    },
-    filename: function (req, file, cb) {
-        // fileName = Date.now() + path.extname(file.originalname)
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-});
+// const upload = multer({ storage: storage });
 
-const upload = multer({ storage: storage });
+// router.get('/upload', (req, res) => res.send('File upload API'))
 
-router.get('/upload', (req, res) => res.send('File upload API'))
+// router.put("/uploadfile", upload.single("file"), uploadFile);
 
-router.put("/uploadfile", upload.single("file"), uploadFile);
-
-function uploadFile(req, res) {
-    const host = req.hostname;
-    const filePath = req.protocol + "://" + host + '/' + req.file.path;
+// function uploadFile(req, res) {
+//     const host = req.hostname;
+//     const filePath = req.protocol + "://" + host + '/' + req.file.path;
     
-    const fileUpload = new FileUploadModel({
-        file: req.file.filename,
-        filePath: filePath
-    })
+//     const fileUpload = new FileUploadModel({
+//         file: req.file.filename,
+//         filePath: filePath
+//     })
     
-    fileUpload
-        .save()
-        .then(file => {
-            console.log(file);
-            res.json({ message: "Successfully uploaded file", data: req.file });
-        })
+//     fileUpload
+//         .save()
+//         .then(file => {
+//             console.log(file);
+//             res.json({ message: "Successfully uploaded file", data: file });
+//         })
 
-}
+// }
 
 
 // router.put('/uploadfile', upload.single('file'), (req, res) => {
